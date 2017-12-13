4a0a3f7ebd3b5eecf945cb13b26072c41837521c
Austin Schaffer <aschaffer@atlas-sys.com>
2017-12-13 08:31:37 -0500

    Moved show_action? into RecordMapper superclass

--- public/lib/record_mapper.rb
+++ public/lib/record_mapper.rb
@@ -15,6 +15,41 @@ class RecordMapper
     end
 
     def show_action?
+        begin
+            if self.repo_settings
+
+                puts "-- Aeon Plugin checking for top containers."
+                has_top_container = false
+
+                if self.record['json'].present? && self.record['json']['instances'].present?
+                    self.record['json']['instances'].each do |instance|
+
+                        sub_container = instance.dig('sub_container')
+                        next if !sub_container
+
+                        top_container_uri = sub_container.dig('top_container', 'ref')
+
+                        if top_container_uri && !top_container_uri.blank?
+                            has_top_container = true
+                        end
+                    end
+                end
+
+                only_top_containers = self.repo_settings[:requests_permitted_for_containers_only] || false
+
+                puts "-- Aeon Plugin Containers found? #{has_top_container}"
+                puts "-- Aeon Plugin only_top_containers? #{only_top_containers}"
+
+                return (has_top_container || !only_top_containers)
+            end
+
+        rescue Exception => e
+            puts "Failed to create Aeon Request action."
+            puts e.message
+            puts e.backtrace.inspect
+
+        end
+
         false
     end
 

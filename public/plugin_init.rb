21f975439092cebe2402d0a1ec75f2731293a2b6
jambun <jambun@gmail.com>
2018-03-29 12:06:15 +1100

    Back out test for existing custom actions when initializing config

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,9 +1,7 @@
 ## Register our custom page action
-unless AppConfig.has_key?(:pui_page_custom_actions)
-  AppConfig[:pui_page_custom_actions] << {
-    'record_type' => ['archival_object', 'accession'],
-    'erb_partial' => 'aeon/aeon_request_action'
-  }
-end
+AppConfig[:pui_page_custom_actions] << {
+  'record_type' => ['archival_object', 'accession'],
+  'erb_partial' => 'aeon/aeon_request_action'
+}
 
 

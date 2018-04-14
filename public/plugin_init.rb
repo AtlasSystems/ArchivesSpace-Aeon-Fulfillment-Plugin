8485b2c4f732a070b1456f1eb59823e479792f7b
jambun <jambun@gmail.com>
2018-04-14 14:01:04 +1000

    Use the provided method to add the button rather than pushing to config, and add support for positioning the button

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,9 +1,9 @@
 ## Register our custom page action
 record_types = AppConfig.has_key?(:aeon_fulfillment_record_types) ? AppConfig[:aeon_fulfillment_record_types]
                                                                   : ['archival_object', 'accession']
-AppConfig[:pui_page_custom_actions] << {
-  'record_type' => record_types,
-  'erb_partial' => 'aeon/aeon_request_action'
-}
 
+button_position = AppConfig.has_key?(:aeon_fulfillment_button_position) ? AppConfig[:aeon_fulfillment_button_position] : nil
 
+Plugins::add_record_page_action_erb(record_types,
+                                    'aeon/aeon_request_action',
+                                    button_position)

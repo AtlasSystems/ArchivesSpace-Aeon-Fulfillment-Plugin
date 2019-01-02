c9ee5aab05ce6cf1adbaaf92079072034334b776
Austin Schaffer <aschaffer@atlas-sys.com>
2019-01-02 16:54:00 -0500

    Simplified tooltip

--- public/assets/js/aeon_request_action.js
+++ public/assets/js/aeon_request_action.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
-    $('[rel="tooltip"]').tooltip()
-    .on("mouseenter", function() {
-        var _this = this;
-        $(this).tooltip("show");
-        $(".tooltip").on("mouseleave", function () {
-            $(_this).tooltip('hide');
-        });
-    })
-    .on("mouseleave", function() {
-        var _this = this;
-        setTimeout(function() {
-            if (!$(".tooltip:hover").length) {
-                $(_this).tooltip("hide");
-            }
-        }, 300);
-    })
+    $('#disabled-button-wrapper').tooltip()
 });

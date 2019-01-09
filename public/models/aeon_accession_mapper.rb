76e44600748511f2dca3472415145b3eee09ebc0
Austin Schaffer <schaffer.austin.t@gmail.com>
2019-01-09 15:46:53 -0500

    Removed inclusion of user defined fields

--- public/models/aeon_accession_mapper.rb
+++ public/models/aeon_accession_mapper.rb
@@ -14,36 +14,7 @@ class AeonAccessionMapper < AeonRecordMapper
             .reject {|id_comp| id_comp.blank?}
             .join('-')
 
-        user_defined = json['user_defined']
-        if user_defined
-            mappings['user_defined_boolean_1'] = user_defined['boolean_1']
-            mappings['user_defined_boolean_2'] = user_defined['boolean_2']
-            mappings['user_defined_boolean_3'] = user_defined['boolean_3']
-            mappings['user_defined_integer_1'] = user_defined['integer_1']
-            mappings['user_defined_integer_2'] = user_defined['integer_2']
-            mappings['user_defined_integer_3'] = user_defined['integer_3']
-            mappings['user_defined_real_1'] = user_defined['real_1']
-            mappings['user_defined_real_2'] = user_defined['real_2']
-            mappings['user_defined_real_3'] = user_defined['real_3']
-            mappings['user_defined_string_1'] = user_defined['string_1']
-            mappings['user_defined_string_2'] = user_defined['string_2']
-            mappings['user_defined_string_3'] = user_defined['string_3']
-            mappings['user_defined_string_4'] = user_defined['string_4']
-            mappings['user_defined_text_1'] = user_defined['text_1']
-            mappings['user_defined_text_2'] = user_defined['text_2']
-            mappings['user_defined_text_3'] = user_defined['text_3']
-            mappings['user_defined_text_4'] = user_defined['text_4']
-            mappings['user_defined_text_5'] = user_defined['text_5']
-            mappings['user_defined_date_1'] = user_defined['date_1']
-            mappings['user_defined_date_2'] = user_defined['date_2']
-            mappings['user_defined_date_3'] = user_defined['date_3']
-            mappings['user_defined_enum_1'] = user_defined['enum_1']
-            mappings['user_defined_enum_2'] = user_defined['enum_2']
-            mappings['user_defined_enum_3'] = user_defined['enum_3']
-            mappings['user_defined_enum_4'] = user_defined['enum_4']
-        end
-
-        return mappings
+        mappings
     end
 
     # Returns a hash that maps from Aeon OpenURL values to values in the provided record.
